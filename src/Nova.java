e4ef43f07b415c6f037d9485177d9131682e2c3b
Stefan Zec <stefanzec@hotmail.rs>
2019-12-16 22:28:12 +0100

    #lagano

--- src/Nova.java
+++ src/Nova.java
@@ -4,7 +4,13 @@ public class Nova {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Stefan zeke njekeee");
-		System.out.println("Stefan zeke njekeee");
+		
+		
+	      int x=5;
+	      
+	      System.out.println(x);
+		
+		
 	}
 
 }
