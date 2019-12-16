f9d0bcee02359ee70595f6f766412b209ae847da
neman <nemanjadragutinovic54@gmail.com>
2019-12-16 21:49:04 +0100

    #izmena

--- src/Nova.java
+++ src/Nova.java
@@ -3,7 +3,8 @@ public class Nova {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Stefan zeke njekeee");
+		System.out.println("Stefan zeke njekeee");
 	}
 
 }
